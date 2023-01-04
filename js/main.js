8b08d2942b7e719775b8337c27f191d2ce301fb5
rokeshkpt <90515322+rokeshkpt@users.noreply.github.com>
2023-01-04 23:19:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+        //layoutMode: 'fitRows'
     });
     $('#portfolio-flters li').on('click', function () {
         $("#portfolio-flters li").removeClass('active');
